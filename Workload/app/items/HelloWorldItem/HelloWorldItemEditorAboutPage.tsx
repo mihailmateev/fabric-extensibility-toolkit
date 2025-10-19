e5a8e5de40565defb9770f0ba2afa2c55fd63fea
Gerd Saurer <gerd.saurer@microsoft.com>
2025-10-19 19:35:03 +0200

    fixing About page styling issue

--- Workload/app/items/HelloWorldItem/HelloWorldItemEditorAboutPage.tsx
+++ Workload/app/items/HelloWorldItem/HelloWorldItemEditorAboutPage.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { PageProps } from '../../App';
+import { useTranslation } from 'react-i18next';
+import '../../styles.scss'; // Import generic settings panel styles
+import './HelloWorldItem.scss'; // Import HelloWorld-specific overrides
 
 export function HelloWorldItemEditorAboutPage(props: PageProps) {
+    const { t } = useTranslation();
+    
     return (
-        <div>
-            This is a custom about page for the HelloWorld Item Editor.
-            <br />
-            You can use this page to provide information about the item editor, its features, and how to use it.            
-            
+        <div className="item-settings-panel-container hello-world-settings-panel-container">
+            <div className="item-settings-panel-content">
+                <span className="item-settings-placeholder-text">
+                {t('About_PlaceholderText', 'Your content will appear here')}
+                </span>
+            </div>
         </div>
     );
 }
